0bc459ea0b147dd727128a0d3188b8ef7f40a773
FRC 3958 Controls <robotics3958@students.bchigh.edu>
2019-10-16 10:35:22 -0700

    speed changed to 0.20

--- jellebot/src/main/java/frc/robot/commands/JOperate.java
+++ jellebot/src/main/java/frc/robot/commands/JOperate.java
@@ -40,9 +40,9 @@ public class JOperate extends Command {
         }
 
         if (m_controller.getBumper(Hand.kLeft)) {
-            Robot.m_arm.setArm(0.05);
+            Robot.m_arm.setArm(0.20);
         } else if (m_controller.getBumper(Hand.kRight)) {
-            Robot.m_arm.setArm(-0.05);
+            Robot.m_arm.setArm(-0.20);
         } else {
             Robot.m_arm.setArm(0);
         }
